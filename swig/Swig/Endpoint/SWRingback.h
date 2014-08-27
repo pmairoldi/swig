653e7f135f87b88d28c66ebcc31310b9c4ae9397
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2014-08-27 16:53:26 -0400

    ringback class

---
+++ swig/Swig/Endpoint/SWRingback.h
@@ -0,0 +1,13 @@
+//
+//  SWRingback.h
+//  swig
+//
+//  Created by Pierre-Marc Airoldi on 2014-08-27.
+//  Copyright (c) 2014 PeteAppDesigns. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SWRingback : NSObject
+
+@end
